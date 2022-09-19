19209bcb0615cc56b4dac44c07c63716e3839825
Ginber Candelario <ginber.candelario@gmail.com>
2022-09-19 10:27:27 +0800

    Add my portfolio

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function showEsirms() {
+	document.getElementById("myProjectsLabel").style.display="none";
+	document.getElementById("carouselcontainer").style.display="block";
+	document.getElementById("esirms").style.display="block";
+	document.getElementById("numbertotext").style.display="none";
+	document.getElementById("era").style.display="none";
+	document.getElementById("closeButton").style.display="block";
+	document.getElementById("projectCards").style.display="none";
+}
+
+function showNumbertotext() {
+	document.getElementById("myProjectsLabel").style.display="none";
+	document.getElementById("carouselcontainer").style.display="block";
+	document.getElementById("esirms").style.display="none";
+	document.getElementById("numbertotext").style.display="block";
+	document.getElementById("era").style.display="none";
+	document.getElementById("closeButton").style.display="block";
+	document.getElementById("projectCards").style.display="none";
+}
+
+function showEra() {
+	document.getElementById("myProjectsLabel").style.display="none";
+	document.getElementById("carouselcontainer").style.display="block";
+	document.getElementById("esirms").style.display="none";
+	document.getElementById("numbertotext").style.display="none";
+	document.getElementById("era").style.display="block";
+	document.getElementById("closeButton").style.display="block";
+	document.getElementById("projectCards").style.display="none";
+}
+
+function closeCarousel() {
+	document.getElementById("myProjectsLabel").style.display="block";
+	document.getElementById("carouselcontainer").style.display="none";
+	document.getElementById("closeButton").style.display="none";
+	document.getElementById("projectCards").style.display="block";
+}
